5d11f80afcf6cded4184e2baf1f3c8b7386369d9
Ani <aniruddhyagoswami6@gmail.com>
2026-01-20 14:32:42 +0530

    jjj

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,24 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),
-],
+  plugins: [react(), tailwindcss()],
+  publicDir: 'public', // Ensures Vite looks into the public folder
+  build: {
+    outDir: 'dist',
+    copyPublicDir: true, // Guarantees _redirects is copied to dist/
+    chunkSizeWarningLimit: 1000, // Raises limit to 1MB to reduce warnings
+    rollupOptions: {
+      output: {
+        // Optimization: Splitting large libraries into their own files
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('@mui')) return 'vendor-mui';
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('gsap')) return 'vendor-gsap';
+            return 'vendor'; // all other dependencies
+          }
+        },
+      },
+    },
+  },
 })
